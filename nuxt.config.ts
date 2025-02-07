166af2f5cb8f06553983f24730dd7bc03e4d425a
AdieCode <Adiecode4@gmail.com>
2025-02-07 08:34:17 +0200

    made some global styling changes :)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/global.css'],
   modules: ["@pinia/nuxt"],
   compatibilityDate: "2025-01-21",
   runtimeConfig: {
